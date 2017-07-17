030d8106daf44ff6dcaca72e0e29376df99864b5
haobogao <haobo.gao@qq.com>
2017-07-17 18:00:16 +0800

    submit

--- lcd/lcd_drv.c
+++ lcd/lcd_drv.c
@@ -22,64 +22,83 @@
 #include <asm/arch/fb.h>
 
 
-static struct fb_ops fb_ops42440 = {
-	.owner		= THIS_MODULE,
-//	.fb_setcolreg	= atmel_lcdfb_setcolreg,
-	.fb_fillrect	= cfb_fillrect,
-	.fb_copyarea	= cfb_copyarea,
-	.fb_imageblit	= cfb_imageblit,
+
+//Vertical back porch is the number of inactive lines at the start of a frame, after vertical synchronization period.
+#define LCD_VBPD 		2<<24
+#define LCD_LINEVAL		(272-1)<<14
+#define LCD_VFPD		2<<6
+#define LCD_VSPW		10
+/*board regs*/
+static struct s3c2440_lcd_regs{
+	/*lcdcon1  */
+	unsigned int LCDCON1;
+	unsigned int LCDCON2;		//0X4D000004
+	unsigned int LCDCON3;
+	unsigned int LCDCON4;		//0X4D00000C
+	unsigned int LCDCON5;		//0X4D000010
+	unsigned int LCDSADDR1;		//0X4D000014
+	unsigned int LCDSADDR2;		//0X4D000018
+	unsigned int LCDSADDR3;		//0X4D00001C
+	unsigned int REDLUT;		//0X4D000020
+	unsigned int GREENLUT;		//0X4D000024
+	unsigned int BLUELUT;		//0X4D000028
+	unsigned int dummy[0x4c-0x28-0x4];		//useless
+    unsigned int DITHMODE;		//0X4D00004C
+    unsigned int  TPAL;			//0X4D000050
+    unsigned int LCDINTPND;		//0X4D000054
+    unsigned int LCDSRCPND;		//0X4D000058
+    unsigned int LCDINTMSK;		//0X4D00005C
+    unsigned int TCONSEL;		//0X4D000060
+
 };
 
 
-static struct fb_info *fd_info42440;
+static struct s3c2440_lcd_struct {
+	 struct fb_info *fd_info42440;
+	 struct s3c2440_lcd_regs lcd_regs;
+	 struct fb_ops fb_ops42440 = {
+	 	.owner		= THIS_MODULE,
+	 //	.fb_setcolreg	= atmel_lcdfb_setcolreg,
+	 	.fb_fillrect	= cfb_fillrect,
+	 	.fb_copyarea	= cfb_copyarea,
+	 	.fb_imageblit	= cfb_imageblit,
+	 };
+};
 
+static struct s3c2440_lcd_struct lcd;
 static int __init lcd_init(void)
 {
 	/* 1. allocate a  fb_info structure */
-	fd_info42440 = framebuffer_alloc(0, NULL);
+	lcd.fd_info42440 = framebuffer_alloc(0, NULL);
 
 	/* 2. set the fucking parameters */
-	strcpy(fd_info42440->fix.id, "2440lcd");
-	fd_info42440->fix.smem_len = 240*320*16/8;
-	fd_info42440->fix.type     = FB_TYPE_PACKED_PIXELS;
-	fd_info42440->fix.visual   = FB_VISUAL_TRUECOLOR; /* TFT */
-	fd_info42440->fix.line_length = 240*2;
+	strcpy(lcd.fd_info42440->fix.id, "2440lcd");
+	lcd.fd_info42440->fix.smem_len = 272*480*16/8;
+	lcd.fd_info42440->fix.type     = FB_TYPE_PACKED_PIXELS;
+	lcd.fd_info42440->fix.visual   = FB_VISUAL_TRUECOLOR;
 
-	fd_info42440->var.xres           = 240;
-	fd_info42440->var.yres           = 320;
-	fd_info42440->var.xres_virtual   = 240;
-	fd_info42440->var.yres_virtual   = 320;
-	fd_info42440->var.bits_per_pixel = 16;
+	lcd.fd_info42440->var.xres           = 480;
+	lcd.fd_info42440->var.yres           = 272;
+	lcd.fd_info42440->var.xres_virtual   = 480;
+	lcd.fd_info42440->var.yres_virtual   = 272;
+	lcd.fd_info42440->var.bits_per_pixel = 16;
 
 	/* RGB:565 */
-	fd_info42440->var.red.offset     = 11;
-	fd_info42440->var.red.length     = 5;
+	lcd.fd_info42440->var.red.offset     = 11;
+	lcd.fd_info42440->var.red.length     = 5;
 
-	fd_info42440->var.green.offset   = 5;
-	fd_info42440->var.green.length   = 6;
+	lcd.fd_info42440->var.green.offset   = 5;
+	lcd.fd_info42440->var.green.length   = 6;
 
-	fd_info42440->var.blue.offset    = 0;
-	fd_info42440->var.blue.length    = 5;
+	lcd.fd_info42440->var.blue.offset    = 0;
+	lcd.fd_info42440->var.blue.length    = 5;
 
-	fd_info42440->var.activate       = FB_ACTIVATE_NOW;
+	lcd.fd_info42440->var.activate       = FB_ACTIVATE_NOW;
+	lcd.fd_info42440->var;
 
-
-	/* 2.3 设置操作函数 */
-	fd_info42440->fbops              = &fb_ops42440;
-
-	/* 2.4 其他的设置 */
-	//fd_info42440->pseudo_palette =; //
-	//fd_info42440->screen_base  = ;  /* 显存的虚拟地址 */
-	fd_info42440->screen_size   = 240*324*16/8;
-
-	/* 3. 硬件相关的操作 */
-	/* 3.1 配置GPIO用于LCD */
-	/* 3.2 根据LCD手册设置LCD控制器, 比如VCLK的频率等 */
-	/* 3.3 分配显存(framebuffer), 并把地址告诉LCD控制器 */
-	//fd_info42440->fix.smem_start = xxx;  /* 显存的物理地址 */
-
-	/* 4. 注册 */
-	register_framebuffer(fd_info42440);
+	/*set HW reg*/
+	lcd.lcd_regs.LCDCON1 =4<<8|0x03<<5|0x0c<<1;
+	lcd.lcd_regs.LCDCON2 = VBPD;
 
 	return 0;
 }
